15a1bce40f50e9f115eca3b7688de20d12e2212a
MuhammedDuzgun <muhammedduzgun00@gmail.com>
2024-10-04 23:01:13 +0300

    add getEmployeeById to Controller

--- src/main/java/com/demo/ems_backend/controller/EmployeeController.java
+++ src/main/java/com/demo/ems_backend/controller/EmployeeController.java
@@ -4,10 +4,7 @@ import com.demo.ems_backend.dto.EmployeeDto;
 import com.demo.ems_backend.service.impl.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -23,6 +20,12 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee =  employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable(name = "id") Long id) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(id);
+        return new ResponseEntity<>(employeeDto, HttpStatus.OK);
     }
 
 }
